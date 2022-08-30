bbfcd01fdf7e96c667cebbca5001a0db5cefab12
afds4567 <afds4567@gmail.com>
2022-08-31 00:20:32 +0900

    createDOM + creeateElement

---
+++ src/react.js
@@ -0,0 +1,25 @@
+// import { createElement } from "react";
+
+export function createDOM(node){
+  if (typeof node === 'string') {
+    //console.log(document.createTextNode(node),"return")
+    return document.createTextNode(node)
+  }
+  const element = document.createElement(node.tag);
+  Object.entries(node.props)
+    .forEach(([name,value])=>element.setAttribute(name,value))
+  // console.log("a",element, i,node.children)
+  // i+=1;
+  node.children
+    .map(createDOM)
+    .forEach(i=>element.appendChild(i));//element.appendChild.bind(element)
+  //element.appendChild.bind(element)
+  //console.log("b","elemet:",element, i," node:",node)
+  return element;
+}
+export function createElement(tag, props, ...children) {
+  return{tag,props,children}
+}
+export function render(vdom, container) {
+  container.appendChild(createDOM(vdom))
+}
