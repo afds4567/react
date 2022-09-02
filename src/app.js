3fe134deec744702280f04a4099675b13bc64088
afds4567 <afds4567@gmail.com>
2022-09-02 18:13:57 +0900

    ⭐feat: implement class Componentnent

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 /* @jsx createElement */
-import { createDOM, render,createElement } from "./react";
+import { createDOM, render,createElement, Component } from "./react";
 
-function Title(props) {
-  return <h1>{props.children}</h1>
+class Title extends Component{
+  render() {
+    return <h1>{this.props.children}</h1>
+  }
 }
-
+// function Title(props) {
+//   return <h1>{props.children}</h1>
+// }
 function Item(props) {
   return <li style={`color:${props.color}`}>{props.children}</li>
 }
-const vdom3 = <p>
+const App = () => <p>
   <Title>React 만들기</Title>
   <ul>
     <Item color="red">첫 번째 아이템</Item>
@@ -16,7 +20,7 @@ const vdom3 = <p>
     <Item color="green">세 번째 아이템</Item>
   </ul>
 </p>
-render(vdom3,document.querySelector('#root'))
+render(<App />,document.querySelector('#root'))
 
 //JSX 태그의 이름이 대문자로 시작되면 문자열로 취급하지 않고 '자바스크립트'값으로 취급
 //Ex) React.createElement("p",null,React.createElement(hello,null))
--- src/react.js
+++ src/react.js
@@ -1,37 +1,56 @@
 // import { createElement } from "react";
-
-export function createDOM(node){
+export class Component{
+  constructor(props) {
+    this.props = props
+  }
+}
+export function createDOM(node) {
   if (typeof node === 'string') {
     //console.log(document.createTextNode(node),"return")
     return document.createTextNode(node)
   }
   const element = document.createElement(node.tag);
-  Object.entries(node.props)
-    .forEach(([name,value])=>element.setAttribute(name,value))
+  Object?.entries(node.props)
+    .forEach(([name, value]) => element.setAttribute(name, value))
   // console.log("a",element, i,node.children)
   // i+=1;
   node.children
     .map(createDOM)
-    .forEach(i=>element.appendChild(i));//element.appendChild.bind(element)
+    .forEach(i => element.appendChild(i)); //element.appendChild.bind(element)
   //element.appendChild.bind(element)
   //console.log("b","elemet:",element, i," node:",node)
   return element;
 }
+
+function makeProps(props, children) {
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children,
+  };
+}
+
 export function createElement(tag, props, ...children) {
   props = props || {}
   if (typeof tag === 'function') {
-    if (children.length > 0) {
-      return tag({
-        ...props,
-        children:children.length===1?children[0] : children,
-      })
+    if (tag.prototype instanceof Component) {
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
     } else {
-      return tag(props);
+      if (children.length > 0) {
+        return tag(makeProps(props, children))
+      } else {
+        return tag(props);
+      }
     }
   } else {
-    return { tag, props, children };
+      return {
+        tag,
+        props,
+        children
+      };
+    }
   }
-}
+
 
 export function render(vdom, container) {
   container.appendChild(createDOM(vdom))
